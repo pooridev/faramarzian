7c91d650433e1a3ffa5ef45da6605d1ebaa47395
Pooria Faramarzian <p.faramarzian@bluteam.ir>
2024-10-23 23:29:30 +0330

    feat(vite.config): change server port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@styles": path.resolve(__dirname, "./src/styles"),
