ba21b415611d0408e0d8553b294bde778c48d392
Pooria Faramarzian <pooriafaramarzian@gmail.com>
2024-10-24 15:55:17 +0330

    chore(app): add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/portfolio";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <HomePage />
   </StrictMode>
 );
